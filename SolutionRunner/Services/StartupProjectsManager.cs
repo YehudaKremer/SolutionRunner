59a8fe6b15070c3f8e58b187daa1acdef56bd6ce
Yehuda Kremer <yehudakremer@gmail.com>
2024-11-28 20:20:58 +0200

    add StartupProjectsManager

---
+++ SolutionRunner/Services/StartupProjectsManager.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio.Shell.Settings;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionRunner.Services
+{
+    public class StartupProjectsManager
+    {
+        private readonly WritableSettingsStore settingsStore;
+        private const string CollectionPath = "SolutionRunner";
+        private const string ListPropertyName = "StartupProjects";
+
+        public StartupProjectsManager(IServiceProvider serviceProvider)
+        {
+            var shellSettingsManager = new ShellSettingsManager(serviceProvider);
+            settingsStore = shellSettingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+
+            if (!settingsStore.CollectionExists(CollectionPath))
+            {
+                settingsStore.CreateCollection(CollectionPath);
+            }
+        }
+
+        public async Task SaveStringListAsync(IEnumerable<string> list)
+        {
+            string json = JsonConvert.SerializeObject(list.OrderBy(s => s));
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            settingsStore.SetString(CollectionPath, ListPropertyName, json);
+        }
+
+        public async Task<List<string>> LoadStringListAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (settingsStore.PropertyExists(CollectionPath, ListPropertyName))
+            {
+                string json = settingsStore.GetString(CollectionPath, ListPropertyName);
+                return JsonConvert.DeserializeObject<List<string>>(json) ?? new List<string>();
+            }
+
+            return new List<string>();
+        }
+    }
+}
